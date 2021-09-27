43d3da390288157ccca1cc8430523e93cd150014
Nikhil Vijayan <nikhil@tokencard.io>
2021-09-27 18:27:09 +0100

    chore: add verbose test output

--- test/unit.ts
+++ test/unit.ts
@@ -3,20 +3,20 @@ import parseNumberFromString from '../src/helpers/parse-number-from-string'
 import parseUnitFromString from '../src/helpers/parse-alphabet-from-string'
 import convert from '../src/helpers/convert'
 
-test('#parseNumberFromString can parse number out of string', t => {
+test('parseNumberFromString - can parse number out of string', t => {
 	t.is(parseNumberFromString('23lb'), 23)
 	t.is(parseNumberFromString('100kg'), 100)
 	t.is(parseNumberFromString  ('10.5kg'), 10.5)
 	t.is(parseNumberFromString('20.123kg'), 20.123)
 })
 
-test('#parseUnitFromString can parse a unit from a string', t => {
+test('parseUnitFromString - can parse a unit from a string', t => {
   t.is(parseUnitFromString('23lb'), 'lb')
   t.is(parseUnitFromString('123.5kg'), 'kg')
   t.is(parseUnitFromString('1.12345l'), 'l')
 })
 
-test('#convert can convert units correctly', t => {
+test('convert - can convert units correctly', t => {
   t.is(convert({
     numberToConvert: 10,
     unit: 'km',
